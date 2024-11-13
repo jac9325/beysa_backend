5bc4f2d30b05c6800793134d7b918325607677ad
EdwinBrayan <184645@unsaac.edu.pe>
2024-11-13 15:36:13 -0500

    se esta subiendo los cambios de UserPermissions y Permissions; Aun falta terminar Permissions - Dev5 - 13/11/2024

---
+++ UserDomain/src/main/java/com/beysa/services/UserDomain/Permissions/PermissionsController.java
@@ -0,0 +1,30 @@
+package com.beysa.services.PermissionsDomain.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.beysa.services.UserDomain.Middlewares.Codes;
+import com.beysa.services.UserDomain.Middlewares.Messages;
+import com.beysa.services.UserDomain.Middlewares.ResponseUtils;
+import com.beysa.services.UserDomain.User.DTO.UserRols;
+
+@CrossOrigin(origins = "http://localhost:4200", originPatterns = "*")
+@RestController
+@RequestMapping("/api/v1/user")
+
+public class UserController {
+
+    Codes codes = new Codes();
+    Messages messages = new Messages();
+    ResponseUtils response = new ResponseUtils();
+
+    @Autowired
+    public UserService userService;
+    
+    
+}
---
+++ UserDomain/src/main/java/com/beysa/services/UserDomain/UserPermissions/UserPermissionsRepository.java
@@ -0,0 +1,10 @@
+package com.beysa.services.UserDomain.UserPermissions;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author HP
+ */
+public interface UserPermissionsRepository extends JpaRepository<UserPermissionsl, Long> {
+}
